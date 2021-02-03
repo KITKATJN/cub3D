f6540a39dd1c780a9533e90b17fcf12790e3a5a8
Ilya <lvutinilyha@gmail.com>
2021-02-03 17:56:52 +0300

    can move player v2

--- parser.c
+++ parser.c
@@ -1,12 +1,87 @@
 #include "./libft/libft.h"
 #include "./get_next_line/get_next_line.h"
+#include "cub3D.h"
 #include <fcntl.h>
 #include <mlx.h>
 
-typedef struct  s_vars {
-        void    *mlx;
-        void    *win;
-}               t_vars;
+void ft_init_player(char **map, t_plr *plr)
+{
+	t_point pos;
+
+	ft_bzero(&pos, sizeof(t_point));
+	while (map[pos.y])
+	{
+		pos.x = 0;
+		while (map[pos.y][pos.x])
+		{
+			if (ft_strchr("WENS",map[pos.y][pos.x]))
+			{
+				plr->x = pos.x * SCALE;
+				plr->y = pos.y * SCALE;
+			}
+			pos.x++;
+		}
+		pos.y++;
+	}
+}
+
+void ft_scale_img(t_win *win, t_point point)
+{
+	t_point end;
+
+	end.x = (point.x + 1) * SCALE;
+	end.y = (point.y + 1) * SCALE;
+	point.x *= SCALE;
+	point.y *= SCALE;
+	while (point.y < end.y)
+	{
+		while (point.x < end.x)
+		{
+			mlx_pixel_put(win->mlx, win->win, point.x++, point.y, 0xFFFFFF);
+		}
+		point.x -= SCALE;
+		point.y++;
+	}
+}
+
+
+void ft_draw_player(t_win *win, t_plr *pl)
+{
+	t_point end;
+	t_plr plr = *pl;
+
+	end.x = plr.x + SCALE;
+	end.y = plr.y + SCALE;
+	while (plr.y < end.y)
+	{
+		while (plr.x < end.x)
+		{
+			mlx_pixel_put(win->mlx, win->win, plr.x++, plr.y, 0x119911);
+		}
+		plr.x -= SCALE;
+		plr.y++;
+	}
+}
+
+void draw_screen(t_all *all)
+{
+	t_point point;
+
+	ft_bzero(&point, sizeof(t_point));
+	while (all->map[point.y])
+	{
+		point.x = 0;
+		while (all->map[point.y][point.x])
+		{
+			if(all->map[point.y][point.x] == '1')
+				ft_scale_img(all->win, point);
+			point.x++;
+		}
+		point.y++;
+	}
+	ft_draw_player(all->win, all->plr);
+}
+
 
 char	**make_map(t_list **head, int size)
 {
@@ -17,9 +92,10 @@ char	**make_map(t_list **head, int size)
 
 	while (tmp)
 	{
-		map[++i] = tmp->content;
+		map[++i] = ft_strdup(tmp->content);
 		tmp = tmp->next;
 	}
+	ft_lstclear(head, &free);
 	i = -1;
 	//printf("HI");
 	//while (map[++i])
@@ -27,53 +103,62 @@ char	**make_map(t_list **head, int size)
 	return (map);
 }
 
-void	write_map(char **map)
+char **ft_read_map(char *argv1)
 {
+	char *str = NULL;
+	t_list *head = NULL;
+	const int fd = open(argv1, O_RDONLY);
 
-	t_vars vars;
-	//t_data  img;
-
-    vars.mlx = mlx_init();
-    vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-    //img.img = mlx_new_image(mlx, 192, 108);
-    //img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-    //                             &img.endian);
-
-    int i = 0;
-    int j = 0;
-    while (map[i][j])
-    {
-        j = 0;
-        while (map[i][j] != '\0')
-        {
-			if (map[i][j] == '1')
-				mlx_pixel_put(vars.mlx, vars.win, i, j, 0x00FF0000);
-			if (map[i][j] == '0')
-				mlx_pixel_put(vars.mlx, vars.win, i, j, 0x0000FF00);
-			if (map[i][j] == 'N')
-				mlx_pixel_put(vars.mlx, vars.win, i, j, 0x00FFFFFF);
-            j++;
-        }
-		j = 0;
-        i++;
-    }
-    //mlx_pixel_put(vars.mlx, vars.win, 0, 0, 0x00FF0000);
-    //mlx_loop_hook(vars.mlx, close, &vars);
-    //my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-    //mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-    mlx_loop(vars.mlx);
-
+	while (get_next_line(fd, &str))
+	{
+		ft_lstadd_back(&head, ft_lstnew(str));
+		str = NULL;
+	}
+	close(fd);
+	ft_lstadd_back(&head, ft_lstnew(str));
+	str = NULL;
+	return (make_map(&head, ft_lstsize(head)));
 }
+
+int key_press(int key, t_all *all)
+{
+	mlx_clear_window(all->win->mlx, all->win->win);
+	if (key == 119)
+		all->plr->y -= 1;
+	if (key == 115)
+		all->plr->y += 1;
+	if (key == 97)
+		all->plr->x -= 1;
+	if (key == 100)
+		all->plr->x += 1;
+	if (key == 53)
+		exit(0);
+	draw_screen(all);
+	return (0);
+}
+
 
 int		main(int argc, char **argv)
 {
-	int      fd = open(argv[1], O_RDONLY);
-	char	  *line = NULL;
-	t_list	*head = NULL;
+	t_win win;
+	t_plr plr;
+	t_all all;
 
-	while (get_next_line(fd, &line) && argc > 0)
-		ft_lstadd_back(&head, ft_lstnew(line));
-	ft_lstadd_back(&head, ft_lstnew(line));
-
-	write_map(make_map(&head, ft_lstsize(head)));
+	if (argc == 2)
+		all.map = ft_read_map(argv[1]);
+	else
+	{
+		ft_putendl_fd("need map", 2);
+		return (-1);
+	}
+	ft_init_player(all.map, &plr);
+	win.mlx = mlx_init();
+	win.win = mlx_new_window(win.mlx, 640, 480, "cubik");
+	win.img = mlx_new_image(win.mlx, 640, 480);
+	//win.addr = mlx_get_data_addr(win.img, &win.bpp, &win.line_l, &win.en);
+	all.plr = &plr;
+	all.win = &win;
+	draw_screen(&all);
+	mlx_hook(win.win, 2, (1L << 0), &key_press, &all);
+	mlx_loop(win.mlx);
 }
